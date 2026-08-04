78ac37ab11917c4f4d7a32bd66d72de6952525a2
prasadthorat18 <prasadthorat552@gmail.com>
2026-08-04 20:03:16 +0530

    optimal for 14

--- Array/Easy/13.Longest-subarray-with-sum-k.cpp
+++ Array/Easy/13.Longest-subarray-with-sum-k.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int brutee(int n,int f, vector<int>& arr){
+int brutee(int n,int key, vector<int>& arr){
 
     int len=0;
     for(int i=0; i<n; i++){
@@ -11,7 +11,7 @@ int brutee(int n,int f, vector<int>& arr){
             for(int k=i; k<=j; k++){
                 sum += arr[k];
             }
-            if(sum==f){
+            if(sum==key){
                 len=max(len, j-i+1);
             } 
         }
@@ -19,7 +19,7 @@ int brutee(int n,int f, vector<int>& arr){
     return len;
 }
 
-int brutee2(int n,int f, vector<int>& arr){
+int brutee2(int n,int key, vector<int>& arr){
 
     int len=0;
     for(int i=0; i<n; i++){
@@ -27,23 +27,70 @@ int brutee2(int n,int f, vector<int>& arr){
         for(int j=i; j<n; j++){
             sum += arr[j];
 
-            if(sum==f){
+            if(sum==key){
                 len=max(len, j-i+1);
             }
         }
     }
+    return len;
+}
+
+
+int Optmial_for_postive_negative_zeros(int n,int key, vector<int>& arr){
+    map<long long, int> mp;
+    long long sum=0;
+    int maxLen=0;
+
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+        if(sum == key){
+            maxLen = max(maxLen, i+1);
+        }
+        long long rem = sum -key;
+        if(mp.find(rem) != mp.end()){
+            int len = i - mp[rem];
+            maxLen = max(maxLen, len);
+        }
+        if(mp.find(sum) == mp.end()){
+            mp[sum]=i;
+        }
+    }
+    return maxLen;
+
+}
+
+
+int optimal_positives_zeros(int n, int key, vector<int>& arr){
+
+    int left=0; int right=0;
+    long long sum=arr[0];
+    int maxLen = 0;
+
+    while(right < n){
+        while(sum>key && right >= left){
+            sum -= arr[left];
+            left++;
+        }
+        if(sum == key ){
+            maxLen=max(maxLen, right-left+1);
+        }
+        right++;
+        if(right < n) sum += arr[right];
+    }
+    return maxLen;
+
 }
 
 int main(){
     int n;
     cin>>n;
 
-    int f;
-    cin>>f;
+    int key;
+    cin>>key;
 
     vector<int> arr(n);
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
-    cout<<brutee2(n,f,arr);
+    cout<<optimal(n,key,arr);
 }
