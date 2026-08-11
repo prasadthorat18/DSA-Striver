c1a8864505d65b708abcc07856a5443cd3858776
prasadthorat18 <prasadthorat552@gmail.com>
2026-08-11 15:52:44 +0530

    majority ele. question solved

--- Array/Medium/3.Majority_element-1.cpp
+++ Array/Medium/3.Majority_element-1.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int brutee(int n, vector<int>& arr){
+int majorityElement_Brutee(int n, vector<int>& arr){
 
     for(int i=0; i<n; i++){
         int cnt=0;
@@ -15,6 +15,51 @@ int brutee(int n, vector<int>& arr){
 
 }
 
+int majorityElement_Better(int n, vector<int>& arr) {
+
+    map<int, int> mpp;
+
+    for(int i=0; i<n; i++){
+        mpp[arr[i]]++;
+    }
+    for(auto it : mpp){
+        if(it.second > n/2) return it.first;
+    }
+    return -1;
+        
+}
+
+// moore's Voting Algorithm
+int majorityElement_Optimal(int n, vector<int>& arr) {
+
+    int cnt=0;
+    int el;
+    for(int i=0 ;i<n; i++){
+        if(cnt == 0){
+            el=arr[i];
+            cnt=1;
+        }
+        else if(arr[i] == el){
+            cnt++;
+        }
+        else{
+            cnt--;
+        } 
+    }
+
+    // int cnt2=0;
+    // for(int i=0; i<n; i++){
+    //     if(arr[i]==el) cnt2++;
+    // }
+    // if(cnt2 > n/2) return el;
+
+    // return -1;
+
+    // the question already state that majority element is always presrent so direct print el
+    
+    return el;
+}
+
 int main(){
     int n;
     cin>>n;
@@ -23,7 +68,7 @@ int main(){
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
-    cout<<brutee(n, arr);
+    cout<<majorityElement_Optimal(n, arr);
 
     
 
