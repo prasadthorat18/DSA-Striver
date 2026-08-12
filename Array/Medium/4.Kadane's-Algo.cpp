1e86943d73aff7f51a5c26fda43b0a255aacb570
prasadthorat18 <prasadthorat552@gmail.com>
2026-08-12 20:42:10 +0530

    this os bullshit

---
+++ Array/Medium/4.Kadane's-Algo.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int Find_max_sum_Brutee(int n, vector<int>& arr){
+
+    int maxi=0;
+    for(int i=0; i<n; i++){
+        for(int j=i; j<n; j++){
+            int sum=0;
+            for(int k=i; k<=j; k++){
+                sum=sum+arr[k];
+            }
+            maxi=max(sum, maxi);
+        }
+    }
+    return maxi;
+
+}
+
+int Find_max_sum_Better(int n, vector<int>& arr){
+
+    int maxi=0;
+    for(int i=0; i<n; i++){
+        int sum=0;
+        for(int j=i; j<n; j++){
+            sum=sum+arr[j];
+
+            maxi=max(sum, maxi);
+        }
+    }
+    return maxi;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    
+
+    
+
+    return 0;
+}
